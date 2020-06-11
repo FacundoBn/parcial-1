98915e9b0e6be019bc329e581fd47186fddfba3c
Facu <facundo.bosnjak@gmail.com>
2020-06-11 18:22:41 -0300

    mi-parcial1

---
+++ src/components/Navbar2.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="src-components-navbar-2">
+    <h1>src-components-navbar-2 Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-navbar-2',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .src-components-navbar-2 {
+
+  }
+</style>
